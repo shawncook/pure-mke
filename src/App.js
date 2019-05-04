df0d08f55336044c9331a55b80602108b362e889
Shawn Cook <shawn@alley.co>
2019-05-04 14:59:04 -0500

    add react fx

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
 import React from 'react';
+import Textsearch from './components/textsearch';
 import data from './data.json';
 import './App.css';
 
-function App() {
-  const recyclables = data.recyclables;
-  const categories = data.categories;
-  return (
-    <div className="App">
-      {0 < categories.length && (
-        <ul className="app__categories">
-          {categories.forEach((item) => {
-            console.log(item.name);
-            return (
-              <li key={item}>
-                {item.name}
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      {0 < recyclables.length && (
-        <ul className="app__recyclables">
-          {recyclables.forEach((item) => {
-            console.log(item.name);
-            return (
-              <li key={item}>
-                {item.name}
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchKeyword: '',
+    };
+  }
+
+  render() {
+    const recyclables = data.recyclables;
+    const categories = data.categories;
+    return (
+      <div
+        className="App"
+      >
+        <Textsearch
+          searchKeyword={this.state.searchKeyword}
+        />
+        {0 < categories.length && (
+          <ul
+            className="app__categories"
+          >
+            {categories.map((item) => {
+              return (
+                <li
+                  key={item}
+                >
+                  {item.name}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {0 < recyclables.length && (
+          <ul
+            className="app__recyclables"
+          >
+            {recyclables.map((item) => {
+              return (
+                <li
+                  key={item}
+                >
+                  {item.name}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/textsearch/index.js
+++ src/components/textsearch/index.js
@@ -1,1 +1,27 @@
-// stub for text search component
+import React from 'react';
+import './textsearch.scss';
+
+/**
+ * Text search component.
+ */
+export default class Textsearch extends React.PureComponent {
+  /**
+   * @returns {object} Component markup.
+   */
+  render() {
+    const {
+      searchKeyword,
+    } = this.props;
+    return (
+      <div className="textsearch">
+        <label for="textsearch">
+          Search
+        </label>
+        <input
+          id="textsearch"
+          value={searchKeyword}
+        />
+      </div>
+    );
+  }
+}
