44e6b46b7878890e19ea971a3b706d383345a2f7
Shawn Cook <shawn@alley.co>
2019-05-04 17:38:00 -0500

    progress

--- src/components/elasticSearch/index.js
+++ src/components/elasticSearch/index.js
@@ -1,45 +1,54 @@
 import React, { Component } from 'react';
-import { ReactiveBase, DataSearch, CategorySearch, ResultCard } from '@appbaseio/reactivesearch';
+import {
+  ReactiveBase,
+  DataSearch,
+  CategorySearch,
+  ResultCard
+} from '@appbaseio/reactivesearch';
+import './search.scss';
 
 class ElasticSearch extends React.Component {
   render() {
     return (
-      <div className="main-container">
+      <div className="search">
         <ReactiveBase
           app="pure-mke"
           credentials="8fOyBlB93:f94865a6-c27a-4d4f-a4b4-e2cc337c6960"
          >
-          Hello from ReactiveSearch!
-
           <DataSearch
-              componentId="search"
-              dataField={[
-                'name',
-                'tags'
-              ]}
-              fuzziness={0}
+            componentId="search"
+            dataField={[
+              'name',
+              'tags'
+            ]}
+            fuzziness={0}
             render={
               ({
-                  loading,            // `true` means the query is still in progress 
-                  error,              // error info
-                  data,               // parsed suggestions by Reactivesearch
-                  rawData,            // unmodified suggestions from Elasticsearch
-                  value,              // the current value in the search
-                  downshiftProps      // downshift props
+                loading,            // `true` means the query is still in progress
+                error,              // error info
+                data,               // parsed suggestions by Reactivesearch
+                rawData,            // unmodified suggestions from Elasticsearch
+                value,              // the current value in the search
+                downshiftProps      // downshift props
               }) => (
-              
-              <>
-              <ul className="test">{data.map(element => {
-                return <li>{element.source.name} <strong>({element.value})</strong> </li>
-              }) }</ul>
-              
-              </>
+                <ul className="search__list">
+                  {data.map((element) => (
+                    <li
+                      key={element.value}
+                      className="search__item"
+                    >
+                      <span
+                        className="search__item-title"
+                      >
+                        {element.source.name}
+                      </span>
+                      {element.value}
+                    </li>
+                  ))}
+                </ul>
               )
             }
-              style={{
-                marginBottom: 20
-              }}
-            />
+          />
         </ReactiveBase>
       </div>
     );
