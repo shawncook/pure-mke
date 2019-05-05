53a85c57b5fb27f0f5b6fbdfa6047fd72ce69477
Jasmine <griffiths.jasmine@gmail.com>
2019-05-04 20:02:47 -0500

    added map

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/home/index";
 import Header from "./components/header/index";
-import Footer from "./components/footer.js/index";
+import Footer from "./components/footer/index";
 import Bottle from "./components/recyclables/bottle";
+import RecyclingMap from "./components/recyclingMap/index";
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/bottle" component={Bottle} />
+          <Route exact path="/recycling-map" component={RecyclingMap} />
         </Switch>
         <Footer />
       </>
