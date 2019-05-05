538286cdc4fec054c8c80dbbdca8b7cd71eb35f6
Jasmine <griffiths.jasmine@gmail.com>
2019-05-04 20:57:13 -0500

    updated hamburger menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,27 +1,53 @@
 import React, { Component } from "react";
 import { slide as HamburgerMenu } from "react-burger-menu";
 import { Link } from "react-router-dom";
-import './menu.scss';
-// import './menu.css';
+import "./menu.scss";
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
+
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
+
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu() {
+    this.setState(state => ({ menuOpen: !state.menuOpen }));
+  }
+
   render() {
     return (
       <HamburgerMenu
+        isOpen={this.state.menuOpen}
+        onStateChange={state => this.handleStateChange(state)}
         bodyClassName={"menu-open"}
         className={"app__menu"}
         burgerButtonClassName={"app__menu-button"}
       >
-        <Link to="/">
+        <Link onClick={() => this.closeMenu()} to="/">
           Home
         </Link>
-        <Link to="/logo-discounts">
+        <Link onClick={() => this.closeMenu()} to="/logo-discounts">
           Logo Discounts
         </Link>
-        <Link to="/recycling-map">
+        <Link onClick={() => this.closeMenu()} to="/recycling-map">
           Recycling Map
         </Link>
-        <Link to="/about-us">
+        <Link onClick={() => this.closeMenu()} to="/about-us">
           About
         </Link>
       </HamburgerMenu>
