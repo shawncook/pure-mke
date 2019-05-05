0154a15b3a2192f5d808809d011d133cd3cfb434
Jasmine <griffiths.jasmine@gmail.com>
2019-05-05 09:57:02 -0500

    update to category to show items

--- src/components/categoryDetail/index.js
+++ src/components/categoryDetail/index.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import Slugify from "../../utils/slugify";
 import "./categoryDetail.scss";
-import { ReactComponent as IconClose } from '../../img/icon-close.svg';
-import Icon from '../../utils/icons';
+import { ReactComponent as IconClose } from "../../img/icon-close.svg";
+import Icon from "../../utils/icons";
 import LinkButton from "../common/linkButton";
-
+import data from "../../data.json";
 
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
 
 /**
  * Category detail component.
@@ -17,15 +18,16 @@ export default class CategoryDetail extends React.PureComponent {
    */
   render() {
     const { category, onCloseCategory } = this.props;
+    const items = data.recyclables.filter(
+      item => item.categories && item.categories.includes(category.slug)
+    );
     return (
       <Fragment>
         <Helmet>
           <title>{category.name} - pureMKE</title>
           <meta name="description" content={category.desc} />
         </Helmet>
-        <div
-          className={`categoryDetail ${Slugify(category.name)}`}
-        >
+        <div className={`categoryDetail ${Slugify(category.name)}`}>
           <button
             className="categoryDetail__close-button"
             onClick={() => onCloseCategory()}
@@ -34,15 +36,18 @@ export default class CategoryDetail extends React.PureComponent {
           </button>
           <div className="categoryDetail__wrapper">
             <div className="categoryDetail__header">
-              <h1 className="categoryDetail__title">
-                {category.name}
-              </h1>
+              <h1 className="categoryDetail__title">{category.name}</h1>
               <div className="categoryDetail__image">
                 <Icon width={100} name={Slugify(category.name)} />
               </div>
             </div>
             <div>
-              <LinkButton to={{ pathname: "/recycling-map", state: { prefilters: ['bags']} }}>
+              <LinkButton
+                to={{
+                  pathname: "/recycling-map",
+                  state: { prefilters: ["bags"] }
+                }}
+              >
                 I want to recycle this!
               </LinkButton>
             </div>
@@ -50,6 +55,18 @@ export default class CategoryDetail extends React.PureComponent {
               className="categoryDetail__desc"
               dangerouslySetInnerHTML={{ __html: category.desc }}
             />
+
+            <div>
+              <h3>Items:</h3>
+              <ul>
+                {items.map(item => (
+                  <Link to={`/item/${item.id}`}>
+                    {" "}
+                    <li key={item.id}>{item.name}</li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </Fragment>
