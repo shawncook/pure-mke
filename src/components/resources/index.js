5e219eadb04f5c2541fc1063468ad210273a25a3
Sage LeGault <sage.marie.legault@gmail.com>
2019-05-05 10:15:40 -0500

    Links on resource page
    linky link link

--- src/components/resources/index.js
+++ src/components/resources/index.js
@@ -13,7 +13,16 @@ export default class Resources extends React.PureComponent {
         <div className="app">
           <p>Content</p>
           <p>
-            <Link to="/">Sample URL</Link>
+            <Link to="https://city.milwaukee.gov/mpw/divisions/operations/environmental/sanitation/DropOff#.XM79l5NKgWo">Drop Off Locations</Link>
+          </p>
+          <p>
+            <Link to="https://city.milwaukee.gov/mpw/divisions/operations/sanitationoperations#.XM79q5NKgWo">Milwaukee Sanitation Services</Link>
+          </p>
+          <p>
+            <Link to="https://city.milwaukee.gov/mpw/divisions/operations/environmental/sanitation/GarbageRecyclingSchedules.htm#.XM79wJNKgWo">Find you Recycling Pick Up Date</Link>
+          </p>
+          <p>
+            <Link to="https://city.milwaukee.gov/recycles">City of Milwaukee Recycling Info Page</Link>
           </p>
         </div>
       </Fragment>
