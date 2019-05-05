25c21176d96647d09a5061ac2a1d014bcd8fa1ea
Jasmine <griffiths.jasmine@gmail.com>
2019-05-05 10:35:26 -0500

    fix

--- src/components/categoryDetail/index.js
+++ src/components/categoryDetail/index.js
@@ -22,12 +22,12 @@ export default class CategoryDetail extends React.PureComponent {
     }
 
     const { onCloseCategory } = this.props;
-    const items = data.recyclables.filter(
-      item => item.categories && item.categories.includes(category.slug)
+    const category = data.categories.find(
+      category => category.id == this.props.match.params.id
     );
 
-    const category = data.categories.find(
-      item => item.id == this.props.match.params.id
+    const items = data.recyclables.filter(
+      item => item.categories && item.categories.includes(category.slug)
     );
     return (
       <Fragment>
@@ -35,45 +35,49 @@ export default class CategoryDetail extends React.PureComponent {
           <title>{category.name} - pureMKE</title>
           <meta name="description" content={category.desc} />
         </Helmet>
-        <div className={`categoryDetail ${Slugify(category.name)}`}>
-          <button
-            className="categoryDetail__close-button"
-            onClick={onCloseCategory}
-          >
-            <IconClose />
-          </button>
-          <div className="categoryDetail__wrapper">
-            <div className="categoryDetail__header">
-              <h1 className="categoryDetail__title">{category.name}</h1>
-              <div className="categoryDetail__image">
-                <Icon width={100} name={Slugify(category.name)} />
-              </div>
-            </div>
-            <div>
-              <LinkButton
-                to={{
-                  pathname: "/recycling-map",
-                  state: { prefilters: ["bags"] }
-                }}
-              >
-                I want to recycle this!
-              </LinkButton>
-            </div>
-            <div
-              className="categoryDetail__desc"
-              dangerouslySetInnerHTML={{ __html: category.desc }}
-            />
+        <div className="app">
+          <div className={`categoryDetail ${Slugify(category.name)}`}>
+            <Link
+              className="categoryDetail__close-button"
+              to="/"
+              onClick={onCloseCategory}
+            >
+              <IconClose />
+            </Link>
 
-            <div>
-              <h3>Items:</h3>
-              <ul>
-                {items.map(item => (
-                  <Link key={item.id} to={`/item/${item.id}`}>
-                    {" "}
-                    <li key={item.id}>{item.name}</li>
-                  </Link>
-                ))}
-              </ul>
+            <div className="categoryDetail__wrapper">
+              <div className="categoryDetail__header">
+                <h1 className="categoryDetail__title">{category.name}</h1>
+                <div className="categoryDetail__image">
+                  <Icon width={100} name={Slugify(category.name)} />
+                </div>
+              </div>
+              <div>
+                <LinkButton
+                  to={{
+                    pathname: "/recycling-map",
+                    state: { prefilters: ["bags"] }
+                  }}
+                >
+                  I want to recycle this!
+                </LinkButton>
+              </div>
+              <div
+                className="categoryDetail__desc"
+                dangerouslySetInnerHTML={{ __html: category.desc }}
+              />
+
+              <div>
+                <h3>Items:</h3>
+                <ul>
+                  {items.map(item => (
+                    <Link key={item.id} to={`/item/${item.id}`}>
+                      {" "}
+                      <li key={item.id}>{item.name}</li>
+                    </Link>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
